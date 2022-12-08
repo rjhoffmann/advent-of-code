41c938567721724f6f795a00440c5cbb362cf580
Ryan Hoffmann <ryan@hoff.pub>
2022-12-07 18:38:23 -0600

    day04 part1 & part2

--- 2022/d03/Program.cs
+++ 2022/d03/Program.cs
@@ -1,8 +1,7 @@
 ﻿var input = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt");
 var data = File.ReadLines(input).ToList();
 
-var part1 = data
-    .Select(d => new
+var part1 = data.Select(d => new
     {
         x = d[..(d.Length / 2)],
         y = d[(d.Length / 2)..],
@@ -10,4 +9,9 @@ var part1 = data
     .Select(d => d.x.Intersect(d.y).Single())
     .Sum(x => char.IsLower(x) ? x - 96 : x - 38);
 
+var part2 = data.Chunk(3)
+    .Select(d => d[0].Intersect(d[1]).Intersect(d[2]).Single())
+    .Sum(x => char.IsLower(x) ? x - 96 : x - 38);
+
 Console.WriteLine(part1);
+Console.WriteLine(part2);
